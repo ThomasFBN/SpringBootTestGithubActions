20865d8900492de0b0d2ec427dfe04b21ebd2cef
ThomasFBN <thni0004@stud.kea.dk>
2024-02-27 14:08:52 +0100

    Initial commit

---
+++ src/main/java/com/example/springboottestgithubactions/SpringBootTestGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottestgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestGithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottestgithubactions/SpringBootTestGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottestgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
